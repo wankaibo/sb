96b144a351720d29e0151cd73523886c73329802
fkbm <1656911995@qq.com>
2026-02-02 14:10:35 +0800

    Update sb.sh

--- sb.sh
+++ sb.sh
@@ -4,5 +4,14 @@ curl -sSL https://dot.net/v1/dotnet-install.sh | bash
 apt install -y libicu-dev git
 echo 'export PATH="$HOME/.dotnet:$PATH"' >> ~/.bashrc
 source ~/.bashrc
+echo '{
+  "runtimeOptions": {
+    "configProperties": {
+      "System.GC.HeapHardLimit": 268435456
+    }
+  }
+}' > runtimeconfig.template.json
 dotnet --info
-echo "安装完成!"
+git clone https://gh-proxy.org/https://github.com/NirvanaTec/Fantnel.git
+dotnet build Fantnel.slnx
+echo "sb"
